28ff2df49330d08ef4d44017fe2766b21452e285
bmstyojaop <bmbdyojaop@gmail.com>
2024-02-08 14:34:39 +0900

    💄 ヘッダーとロゴの追加

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
 export const Header = () => {
-  return <header className='h-8 border border-blue-300 bg-blue-50'>ヘッダー</header>;
+  return (
+    <header className='flex h-10 justify-center border border-blue-300 bg-blue-50 px-3'>
+      <div className='size-full max-w-screen-xl bg-slate-200'>
+        <div className='relative size-full'>
+          <Image src='/logo.png' alt='logo' width={36} height={36} />
+        </div>
+        <div></div>
+      </div>
+    </header>
+  );
 };
