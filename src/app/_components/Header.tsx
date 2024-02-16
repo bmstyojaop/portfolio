ca9c4a35d9e8c63dd3b06e3110853ea398307b94
bmstyojaop <bmbdyojaop@gmail.com>
2024-02-16 09:57:25 +0900

    ✨ シンプルなヘッダーを追加

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,13 +1,49 @@
+'use client';
 import Image from 'next/image';
+import Link from 'next/link';
 
+const navLinks: { label: string; path: string }[] = [
+  {
+    label: 'Blog',
+    path: '/blog',
+  },
+  {
+    label: 'Work',
+    path: '/work',
+  },
+  {
+    label: 'Component',
+    path: '/uiCollection',
+  },
+];
 export const Header = () => {
   return (
-    <header className='flex h-10 justify-center border border-blue-300 bg-blue-50 px-3'>
-      <div className='size-full max-w-screen-xl bg-slate-200'>
-        <div className='relative size-full'>
-          <Image src='/logo.png' alt='logo' width={36} height={36} />
+    // TODO: 背景色をぼかし半透明にする（グラスモーフィズム）
+    <header className='flex h-12 justify-center border px-3'>
+      <div className='flex size-full max-w-screen-xl  justify-between'>
+        <Link href='/' className='relative flex size-full items-center'>
+          <Image src='/logo.png' alt='ロゴ' width={36} height={36} priority />
+          <h2 className='font-serif text-3xl'>タス</h2>
+        </Link>
+        <nav className='hidden size-full sm:block'>
+          <ul className='flex h-full justify-around'>
+            {navLinks.map((item) => {
+              return (
+                <li key={item.label} className='flex items-center'>
+                  <Link href={item.path}>{item.label}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <div className='flex size-full items-center justify-end sm:hidden'>
+          <button type='button' className='z-10 space-y-2 '>
+            <div className='h-0.5 w-8 bg-gray-600' />
+            <div className='h-0.5 w-8 bg-gray-600' />
+            <div className='h-0.5 w-8 bg-gray-600' />
+          </button>
         </div>
-        <div></div>
+        <div className='hidden size-full sm:block'></div>
       </div>
     </header>
   );
