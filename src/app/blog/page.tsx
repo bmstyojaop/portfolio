96dc854c2c9b7c5ea50418690356e989909f616f
bmstyojaop <bmbdyojaop@gmail.com>
2024-02-17 17:53:17 +0900

    Cardコンポーネントの調整

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,23 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+
 const BlogPost = () => {
-  return <>ブログ一覧ページ</>;
+  return (
+    <>
+      <Card className='max-h-48 w-96'>
+        <CardHeader>
+          <CardTitle>ブログのタイトル</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className='line-clamp-3 text-slate-500'>
+            このブログ記事では、最新のテクノロジー動向について深く掘り下げていきます。特に、AIの進化とその社会への影響に焦点を当てています。
+            AI技術は日々進化しており、私たちの生活に多大な影響を与えています。例えば、自動運転車、スマートホームデバイス、パーソナライズされた広告など、AIは多くの面で私たちの生活を便利にしています。
+            しかし、AI技術の進化には懸念も伴います。プライバシーの侵害、職業の自動化による雇用の減少、偏見のあるアルゴリズムによる不公平な判断など、AIがもたらす負の側面についても考える必要があります。
+            この記事では、AIのポジティブな側面と懸念される点の両方を探り、テクノロジーが私たちの未来にどのような影響を与えるかを考察します。AIの進化がもたらす可能性について、楽観的な視点と慎重な視点の両方から考えていきましょう。
+          </p>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default BlogPost;
